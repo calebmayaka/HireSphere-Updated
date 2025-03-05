ce50594f721915111a63f5c1f38412bc7b0a89a8
Caleb Mayaka <mayakaombogo254@gmail.com>
2025-03-05 09:42:13 +0300

    update

--- base/user/decorators.py
+++ base/user/decorators.py
@@ -1,8 +1,13 @@
 from django.shortcuts import redirect
 from django.http import HttpResponse
 
-
 def unauthenticated_user(view_func):
+    """
+    Decorator to redirect authenticated users to their respective profile pages.
+    If the user is a company, they are redirected to the company profile page.
+    If the user is an applicant, they are redirected to the applicant feed.
+    If the user is not authenticated, the original view function is called.
+    """
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.is_company:
             return redirect('company-profile', request.user.id)
--- base/user/urls.py
+++ base/user/urls.py
@@ -88,7 +88,7 @@ urlpatterns = [
     path('reset-password/',
          auth_views.PasswordResetView.as_view(template_name="user/password-reset.html"),
          name="reset-password"),
-    path('reset-password-sent/',
+     path('reset-password-sent/',
          auth_views.PasswordResetDoneView.as_view(template_name="user/password-reset-sent.html"),
          name="password_reset_done"),
 
